9ee435a2da6340d3152038d733f2304d02cf3600
vishnu tm <vishnumeleppat007@gmail.com>
2020-07-08 19:51:44 +0530

    added feature of get a user details by it's id

--- models/db.js
+++ models/db.js
@@ -55,8 +55,24 @@ const getUsers = (request, response) => {
     })
   }
 
+  /*
+Get the user details by id
+  */
+
+ const getUserById = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
 module.exports = {
 	createUserTable,
     insertUser,
-    getUsers
+    getUsers,
+    getUserById
 };
--- router/user.js
+++ router/user.js
@@ -1,11 +1,10 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
+const db = require('../models/db');
 
-const db= require('../models/db')
+router.post('/signup', db.insertUser);
+router.get('/users', db.getUsers);
+router.get('/users/:id',db.getUserById)
 
-
-router.post('/signup',db.insertUser);
-router.get('/users',db.getUsers)
-
-module.exports = router
+module.exports = router;
